8569aac376d4555aaafb355fc24e1f04062652fc
laphecet <laurent.aphecetche@cern.ch>
2016-04-28 13:30:36 +0200

    Helper script to unlink things while developping the install script...

---
+++ install/unlink.sh
@@ -0,0 +1,13 @@
+rm -f ~/.tmux.conf
+rm -rf ~/.config
+rm -rf ~/.vim*
+rm -f ~/.zprezto
+rm -f ~/.dev.tmux.conf
+rm -f ~/.zlogin
+rm -f ~/.zlogout
+rm -f ~/.zprezto
+rm -f ~/.zpreztorc
+rm -f ~/.zprofile
+rm -f ~/.zshenv
+rm -f ~/.zshrc
+
