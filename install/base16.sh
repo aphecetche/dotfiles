457f61ad548c4434a8d37ee80160db48a980b8e6
laphecet <laurent.aphecetche@cern.ch>
2017-01-25 12:30:29 +0100

    Need also base16 for iTerm2 if on Mac

--- install/base16.sh
+++ install/base16.sh
@@ -13,3 +13,15 @@ else
 fi
 
 ln -sf $HOME/github.com/chriskempson/base16-shell $DOTFILES/
+
+if [ "$(uname)" = "Darwin" ]; then
+    # base16 for iterm2
+    if ! test -d ~/github.com/chriskempson/base16-iterm2; then
+        git clone https://github.com/chriskempson/base16-iterm2 ~/github.com/chriskempson/base16-iterm2
+    else
+        pushd $HOME/github.com/chriskempson/base16-iterm2
+        git pull 2>&1 > /dev/null
+        popd
+    fi
+fi
+
