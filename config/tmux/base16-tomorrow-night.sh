d7e37b27c80bff807e518e77a33e48f813ef064b
laphecet <laurent.aphecetche@cern.ch>
2017-01-26 21:34:18 +0100

    adding 2 tmux themes

---
+++ config/tmux/base16-tomorrow-night.sh
@@ -0,0 +1,49 @@
+# 
+## To find out the colour used by the current base16 theme
+# use the base16-shell/colortest utility
+
+# default statusbar colors
+set-option -g status-bg colour18
+set-option -g status-fg colour07
+set-option -g status-attr default
+
+# default window title colors
+set-window-option -g window-status-fg colour20 
+set-window-option -g window-status-bg default
+set-window-option -g window-status-attr default
+
+# active window title colors
+set-window-option -g window-status-current-fg colour18
+set-window-option -g window-status-current-bg colour04
+# set-window-option -g window-status-current-attr bright
+
+# pane border
+set-option -g pane-border-fg colour02
+set-option -g pane-border-bg colour00
+set-option -g pane-active-border-fg colour00
+
+# message text
+set-option -g message-bg default
+set-option -g message-fg colour02
+set-option -g message-attr bright
+
+# pane number display
+set-option -g display-panes-active-colour blue #blue
+set-option -g display-panes-colour brightred #orange
+
+# clock
+set-window-option -g clock-mode-colour green #green
+
+# bell
+set-window-option -g window-status-bell-style fg=black,bg=red #base02, red
+
+
+# Status bar
+
+set -g window-status-format "#I #W"
+set -g status-position bottom
+set -g status-left "#[fg=colour01][#S] "
+set -g status-right "#[fg=colour20]Battery:#{battery_percentage} | %a %d %b %Y #[fg=colour01][ %H:%M ]" 
+set -g status-right-length 100
+set -g status-justify left
+
--- install/prezto.sh
+++ install/prezto.sh
@@ -7,17 +7,3 @@ if ! test -d $HOME/github.com/aphecetche/prezto; then
     popd > /dev/null
 fi
 
-ln -s $HOME/github.com/aphecetche/prezto $DOTFILES/
-
-# link for prezto files
-# mind the order, first the $HOME/.zpresto then the rest...
-
-echo -e "\n\nCreating zprezto symlink"
-echo "=============================="
-rm -f $HOME/.zprezto
-ln -s $DOTFILES/prezto $HOME/.zprezto
-for rcfile in $(find "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/ -name z*); do
-    echo "Creating symlink for $rcfile"
-    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.$(basename $rcfile)"
-done
-
