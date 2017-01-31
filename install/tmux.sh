4a345f66e74453cb104e8568e599fbc84a3e96df
laphecet <laurent.aphecetche@cern.ch>
2017-01-31 23:36:43 +0100

    fix typo

--- install/tmux.sh
+++ install/tmux.sh
@@ -3,7 +3,7 @@
 # Install Tmux Plugin Manager, which takes care of the other plugins
 
 if ! test -d ~/.tmux/plugins/tpm; then
-    mkdir -p n ~/save
+    mkdir -p ~/save
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     ~/.tmux/plugins/tpm/bin/install_plugins
 else
