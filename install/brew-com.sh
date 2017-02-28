4e262b7c704bdcc2996bb6e540e3d58f0fa1effe
laphecet <laurent.aphecetche@cern.ch>
2017-02-28 23:59:29 +0100

    get back to regular mutt as 1.8 has the edit-label command

--- install/brew-com.sh
+++ install/brew-com.sh
@@ -1,4 +1,3 @@
 # "communication" stuff for mac
 
-brew install neomutt/homebrew-neomutt/neomutt --with-sidebar-patch
-brew install mu isync w3m msmtp urlview imapfilter
+brew install mutt mu isync w3m msmtp urlview imapfilter
