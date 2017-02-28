47e7ae6d2c9fabb654846375c13f3fdcfd998a30
laphecet <laurent.aphecetche@cern.ch>
2017-02-28 23:56:48 +0100

    remove git virtual mbox

--- bin/musearches.sh
+++ bin/musearches.sh
@@ -3,7 +3,7 @@
 # mu searches to fake virtual mailboxes in mutt
 #
 
-/usr/local/bin/mu find subject:"[GIT*" --format=links --linksdir=~/Maildir/mu/git --clearlinks
+# /usr/local/bin/mu find subject:"[GIT*" --format=links --linksdir=~/Maildir/mu/git --clearlinks
 
 /usr/local/bin/mu find m:"/cern/Sent Messages" or m:"/subatech/Sent Messages" or m:"/gmail/Sent Messages" or m:"/icloud/Sent Messages" --format=links --linksdir=~/Maildir/mu/sent --clearlinks
 
