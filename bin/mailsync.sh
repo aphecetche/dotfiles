f82481826e43739d308f4dba9e29147188692a41
laphecet <laurent.aphecetche@cern.ch>
2017-03-05 16:19:40 +0100

    update

--- bin/mailsync.sh
+++ bin/mailsync.sh
@@ -3,12 +3,50 @@
 # script to sync imap stuff
 # = imapfilter + mbsync + mu index
 
+EMAIL_CONN_TEST=n
+
+# connection test (inspired) from the mstmpq script
+#   EMAIL_CONN_TEST 
+#                       if =p or unset will use a ping test (debian.org) for a connection
+#                       if =P will use a fast ping test (8.8.8.8) for a connection
+#                       if =n will use netcat (nc) to test for a connection
+# test whether system is connected
+# returns t/f (0/1)
+#
+
 sync=$1
 shift
+filters=$@
+
+connect_test() {
+  if [ -z "$EMAIL_CONN_TEST" ] || \
+     [ "$EMAIL_CONN_TEST" = 'p' ] ; then                       # use ping test (default)
+    # verify net connection - ping ip address of debian.org
+    # would ping -qnc2 -w4 be better ?
+    # would ping -qnc1 -w10 or -w20 be better ?
+    #ping -qnc1 -w4 debian.org >/dev/null 2>&1 || return 1
+    ping -qnc2 -w10 debian.org >/dev/null 2>&1 || return 1
+
+  elif [ "$EMAIL_CONN_TEST" = 'P' ] ; then                     # use quicker ping test
+    # I personally think that including a DNS lookup
+    # is a better connection test but some
+    # have found the above test too slow
+    ping -qnc1 -w4 8.8.8.8 >/dev/null 2>&1 || return 1
+
+  elif [ "$EMAIL_CONN_TEST" = 'n' ] ; then                     # use netcat (nc) test
+    # must, of course, have netcat (nc) installed
+    which nc >/dev/null 2>&1 || \
+      log -e 1 "msmtpq : can't find netcat executable [ nc ]"  # if not found - complain ; quit
+    'nc' -vz www.debian.org 80 >/dev/null 2>&1 || return 1
+  fi
+  return 0
+}
+
+connect_test || { printf "no connection. doing nothing\n" ; exit 1 ; }
 
 dir="$HOME/dotfiles/config/imapfilter"
 
-for filter in $@; do
+for filter in $(printf "$filters"); do
     filterscript="$dir/$filter.lua" 
     if test -f "$filterscript"; then
         pushd $dir > /dev/null
@@ -18,7 +56,4 @@ for filter in $@; do
 done
 
 date && echo "Starting mbsync for $sync" && /usr/local/bin/mbsync $sync && echo "mbsync done" && date
-
-/usr/local/bin/mu index
-
 
--- bin/musearches.sh
+++ bin/musearches.sh
@@ -3,8 +3,18 @@
 # mu searches to fake virtual mailboxes in mutt
 #
 
+/usr/local/bin/mu index
+
 # /usr/local/bin/mu find subject:"[GIT*" --format=links --linksdir=~/Maildir/mu/git --clearlinks
 
+# all sent messages
 /usr/local/bin/mu find m:"/cern/Sent Messages" or m:"/subatech/Sent Messages" or m:"/gmail/Sent Messages" or m:"/icloud/Sent Messages" --format=links --linksdir=~/Maildir/mu/sent --clearlinks
 
+# combined subatech+cern inbox
 /usr/local/bin/mu find m:/cern/INBOX or m:/subatech/INBOX --format=links --linksdir=~/Maildir/mu/inbox-work --clearlinks
+
+# today, this week, this month
+/usr/local/bin/mu find date:today..now --format=links --linksdir=~/Maildir/mu/today --clearlinks
+/usr/local/bin/mu find date:1w..now --format=links --linksdir=~/Maildir/mu/thisweek --clearlinks
+/usr/local/bin/mu find date:4w..now --format=links --linksdir=~/Maildir/mu/thismonth --clearlinks
+
